b610f584d0c31d37d5302a468d9302187f214e05
MrPorch88 <jorge.velez.moreno@gmail.com>
2018-04-17 00:34:33 +0200

    Se ha Finalizado la estructura General.
    Queda crear el algoritmo para resolver los sistemas y mostrar los pasos
    en la siguiente tab.

--- ProyectoIngenieria/src/sistema/VentanaSistema.java
+++ ProyectoIngenieria/src/sistema/VentanaSistema.java
@@ -4,15 +4,21 @@ import java.awt.Dimension;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import main.MenuPrincipal;
 
 
 public class VentanaSistema extends JPanel{
 
 	private JFrame frame;
+
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -41,24 +47,23 @@ public class VentanaSistema extends JPanel{
 	}
 	
 	public VentanaSistema() {
-		//super(new GridLayout(1, 1));
 		super(new BorderLayout());
 		initialize();
 	}
-	
-	protected JComponent makeTextPanel(String text) {
-        JPanel panel = new JPanel(false);
-        JLabel filler = new JLabel(text);
-        filler.setHorizontalAlignment(JLabel.CENTER);
-        panel.setLayout(new GridLayout(1, 1));
-        panel.add(filler);
-        return panel;
-    }
-	
-
-	
+		
 	public void initialize() {
+		
+		/***
+		 * 
+		 * 
+		 * VENTANA PRINCIPAL
+		 * 
+		 * 
+		 */
+		
+		
 		frame = new JFrame("Sistema de 3 Ecuaciones");
+
 		
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double width = (screenSize.getWidth())/3;
@@ -68,25 +73,106 @@ public class VentanaSistema extends JPanel{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		
-		// Creacion de panel de tabs
+		
+		
+		
+		/***
+		 * 
+		 * 
+		 * DEFINICION DE PANELES
+		 * 
+		 * 
+		 */
+		
+		
+		
 		JTabbedPane tabbedPane = new JTabbedPane();
         
         
-        JPanel panelPestana = new JPanel(); // Panel dentro de la tab
+        JPanel panelPestana1 = new JPanel(); // Panel dentro de la tab
+        JPanel panelPestana2 = new JPanel();
+        
         JPanel panelGeneral = new JPanel(); // Panel dentro del frame principal
         
-		tabbedPane.addTab("Enunciado", panelPestana); // Se crea una nueva tab y se agrega el panel de esta
+        JPanel panelInferiorGeneral = new JPanel();
+
+		
+	
+		
+		/***
+		 * 
+		 * 
+		 * CREACION DE TABS
+		 * 
+		 * 
+		 */
+		
+
+        
+		tabbedPane.addTab("Enunciado", panelPestana1); // Se crea una nueva tab y se agrega el panel de esta
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+
          
         
-        tabbedPane.addTab("Resolucion", new JButton("Hola2"));
+        tabbedPane.addTab("Resolucion", panelPestana2);
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-        
        
-        panelGeneral.add(tabbedPane); // Se agrega al panel principal el de las tabs
-        frame.add(panelGeneral, BorderLayout.CENTER);
+        
+    	
+		/***
+		 * 
+		 * 
+		 * CREACION DE COMPONENTES
+		 * 
+		 * 
+		 */
+        
 		
-   
+        JButton botonVolver = new JButton("Volver");
+		botonVolver.setBounds(0, 0, 888, 888);
+		botonVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				new MenuPrincipal().open();
+			}
+		});
+		
+		JLabel intro = new JLabel("Por favor introduce las variables del sistema de ecuaciones en los siguientes campos:");
+		
+		panelPestana1.add(intro);
+	
+		
+		
+
+
+		
+		String[] colHeadings = {"X","Y","Z", "="};
+		int numFilas = 3 ;
+		DefaultTableModel model = new DefaultTableModel(numFilas, colHeadings.length); // Se crea un modelo para poder hacer uso de los datos
+		model.setColumnIdentifiers(colHeadings);
+		JTable table = new JTable(model);
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+
+		//scrollPane.setViewportView(table);
+		//table.setFillsViewportHeight(true);
+		
+		
+		
+		panelPestana1.setOpaque(true);
+		panelPestana1.add(scrollPane);
+		
+		
+		
+		
+		
+		
+		panelInferiorGeneral.add(botonVolver);
+		panelGeneral.add(tabbedPane); // Se agrega al panel principal
+		
+		frame.add(panelInferiorGeneral, BorderLayout.SOUTH);
+        frame.add(panelGeneral, BorderLayout.WEST); // Se agrega el panel general al conjunto entero, el frame
+        frame.pack();
         frame.setVisible(true);
         
 
