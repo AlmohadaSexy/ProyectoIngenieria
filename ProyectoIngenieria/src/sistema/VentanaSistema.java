a1e0b74c1d20decbc539fcbf02c31ee199679287
Jorge Moreno <jorgemoreno@>
2018-04-16 12:22:19 +0200

    He añadido pestañas y funcionan correctamente. Faltan agrandarlas.

--- ProyectoIngenieria/src/sistema/VentanaSistema.java
+++ ProyectoIngenieria/src/sistema/VentanaSistema.java
@@ -1,14 +1,19 @@
 package sistema;
 
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
-
 import javax.swing.*;
 
-public class VentanaSistema {
+
+public class VentanaSistema extends JPanel{
 
 	private JFrame frame;
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -36,8 +41,21 @@ public class VentanaSistema {
 	}
 	
 	public VentanaSistema() {
+		//super(new GridLayout(1, 1));
+		super(new BorderLayout());
 		initialize();
 	}
+	
+	protected JComponent makeTextPanel(String text) {
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
+    }
+	
+
 	
 	public void initialize() {
 		frame = new JFrame("Sistema de 3 Ecuaciones");
@@ -48,7 +66,30 @@ public class VentanaSistema {
 		
 		frame.setSize((int)width, (int)height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		frame.setLocationRelativeTo(null);		
+		frame.setLocationRelativeTo(null);
+		
+		// Creacion de panel de tabs
+		JTabbedPane tabbedPane = new JTabbedPane();
+        
+        
+        JPanel panelPestana = new JPanel(); // Panel dentro de las tab
+        JPanel panelGeneral = new JPanel(); // Panel dentro del frame principal
+        
+		tabbedPane.addTab("Enunciado", panelPestana); // Se crea una nueva tab y se agrega el panel de esta
+        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+         
+        
+        tabbedPane.addTab("Resolucion", new JButton("Hola2"));
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+        
+       
+        panelGeneral.add(tabbedPane); // Se agrega al panel principal el de las tabs
+        frame.add(panelGeneral, BorderLayout.CENTER);
+		
+   
+        frame.setVisible(true);
+        
+
+
 	}
 }
