d4a1a3190617390b86e7cb36b4c181033ba24d14
AlmohadaSexy <itrigueropedraza@gmail.com>
2018-05-17 13:08:17 +0200

    Arreglados el resize y algunos detalles

--- ProyectoIngenieria/src/poblacion/VentanaPoblacion.java
+++ ProyectoIngenieria/src/poblacion/VentanaPoblacion.java
@@ -11,11 +11,12 @@ public class VentanaPoblacion extends JFrame {
 	GridBagLayout gbl;
 	GridBagConstraints gbc;
 	pedirDatos upperPanel;
-	JPanel downRightPanel, downLeftPanel, downCenterPanel;
+	JPanel panelBotones, panelPrincipal;
 	public static JTabbedPane panelTab;
 	static boolean doubleJustSeemsGood;
 	double k, A, B;
-	int anos, PA, PJ;
+	int PA, PJ;
+	
 	/**
 	 * 
 	 */
@@ -25,33 +26,14 @@ public class VentanaPoblacion extends JFrame {
     	this.setResizable(true);
     	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	this.setVisible(true);
-    	this.getContentPane().setBackground(Color.BLACK);
     	this.setLocationRelativeTo(null);
-    	this.setResizable(false);
-        
-    	// Setting Frame Layout
-    	gbl = new GridBagLayout();
-    	gbl.columnWidths = new int[] {200/3};
-    	gbl.rowHeights = new int[] {Variables.height - 100};
-    	gbl.columnWeights = new double[] {1};
-    	gbl.rowWeights = new double[] {1};
-        
-    	gbc = new GridBagConstraints();
-    	this.setLayout(gbl);
-
-    	gbc.fill = GridBagConstraints.BOTH;
-    	gbc.weightx = 1.0;
-    	gbc.weighty = 1.0;
-
-    	// Add Panels
-    	upperPanel = new pedirDatos();
-        panelTab.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    	this.addPanels(0, 0, 1, 1, upperPanel); // row, col, height, width, component
-
-    	JPanel panelBotones = new JPanel();
-        panelBotones.setLayout(new BoxLayout(panelBotones,BoxLayout.LINE_AXIS));
-        
     	
+    	//Creamos los 3 paneles, el principal, y los dos que van a ir dentro
+    	panelPrincipal = new JPanel();
+    	upperPanel = new pedirDatos();
+    	upperPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    	panelBotones = new JPanel();
+        
     	JButton botonAtras = new JButton("Atras");
     	botonAtras.setFont(new Font("Tahoma", Font.PLAIN, 26));
     	botonAtras.addActionListener(new ActionListener() {
@@ -59,9 +41,8 @@ public class VentanaPoblacion extends JFrame {
     			mainPoblacion.close();
     		}
     	});
-    	panelBotones.add(botonAtras, BorderLayout.CENTER);
         
-    	JButton botonSubmit = new JButton("Submit");
+    	JButton botonSubmit = new JButton("Aceptar");
     	botonSubmit.setFont(new Font("Tahoma", Font.PLAIN, 26));
     	botonSubmit.addActionListener(new ActionListener() {
     		public void actionPerformed(ActionEvent e) {
@@ -90,34 +71,31 @@ public class VentanaPoblacion extends JFrame {
         			panelTab.setSelectedIndex(Variables.contOperaciones);
         			Variables.contOperaciones++;
     			} else if (panelTab.getSelectedIndex() != 0){
-    				JOptionPane.showMessageDialog(null, "Vaya a la primera TAB para poder calcular el resul0tado");
+    				panelTab.setSelectedIndex(0);
+    				JOptionPane.showMessageDialog(null, "Debe estar en la primera TAB para enviar los datos.");
     			}
     		}
     	});
-    	panelBotones.add(botonSubmit, BorderLayout.CENTER);
 		
-    	JButton botonClear = new JButton("Clear");
+    	JButton botonClear = new JButton("Cerrar TAB y limpiar");
     	botonClear.setFont(new Font("Tahoma", Font.PLAIN, 26));
     	botonClear.addActionListener(new ActionListener() {
     		public void actionPerformed(ActionEvent e) {
-    			if(panelTab.getSelectedIndex() == 0) {
-    				pedirDatos.textFieldK.setText("");
-    				pedirDatos.textFieldA.setText("");
-        			pedirDatos.textFieldB.setText("");
-        			pedirDatos.textFieldPA.setText("");
-        			pedirDatos.textFieldPJ.setText("");
-        			pedirDatos.textFieldNombre.setText("");
-    			} else {
-    				JOptionPane.showMessageDialog(null, "Vaya a la pestaña de Datos para borrarlo todo");
-    			}
+    			
+    			pedirDatos.textFieldK.setText("");
+    			pedirDatos.textFieldA.setText("");
+        		pedirDatos.textFieldB.setText("");
+        		pedirDatos.textFieldPA.setText("");
+        		pedirDatos.textFieldPJ.setText("");
+        		pedirDatos.textFieldNombre.setText("");
+    			panelTab.removeTabAt(panelTab.getSelectedIndex());
+    			panelTab.setSelectedIndex(0);
     		}
     	});
-    	panelBotones.add(botonClear, BorderLayout.CENTER);
-    	
-    	
-    	panelBotones.setBorder(
-                BorderFactory.createEmptyBorder(0, 10, 5, 10));
-        
+
+    	//Ponemos el layout al panelBotones y añadimos los respectivos botones
+    	panelBotones.setLayout(new BoxLayout(panelBotones,BoxLayout.LINE_AXIS));
+    	panelBotones.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 5));
     	panelBotones.add(Box.createHorizontalGlue());
     	panelBotones.add(botonSubmit);
         panelBotones.add(Box.createRigidArea(new Dimension (5,0)));
@@ -125,26 +103,10 @@ public class VentanaPoblacion extends JFrame {
         panelBotones.add(Box.createRigidArea(new Dimension (5,0)));
         panelBotones.add(botonAtras);
         
-        this.addPanels(1, 0, 1, 1, panelBotones);
-	}
-	/**
-	 * 
-	 * Este metodo se usa para organizar los elementos dentro del frame
-	 * 
-	 * @param row Fila en la que se encuentra
-	 * @param col Columna en la que se encuentra
-	 * @param height Numero de filas que ocupa
-	 * @param width Numero de columnas que ocupa
-	 * @param com Componente que añades a la ventana
-	 * 
-	 * @author IgnacioT
-	 */
-	private void addPanels(int row, int col, int height, int width, Component com) {
-		gbc.gridx = col;
-		gbc.gridy = row;
-		gbc.gridheight = height;
-		gbc.gridwidth = width;
-		gbl.setConstraints(com, gbc);
-		this.getContentPane().add(com);
+        //Ponemos el layout al panel principal y añadimos upperPanel y panelBotones
+        panelPrincipal.setLayout(new BorderLayout());
+        panelPrincipal.add(upperPanel);
+        panelPrincipal.add(panelBotones, BorderLayout.PAGE_END);
+        this.add(panelPrincipal);
 	}
 }
